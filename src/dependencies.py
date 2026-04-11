e30251d380c842a62347029da4a521b26c6e6e83
Eduardo Lima <eduefi@gmail.com>
2026-04-11 22:09:11 +0200

    make item repository a singleton

--- src/dependencies.py
+++ src/dependencies.py
@@ -8,9 +8,10 @@ from src.domain.ports.item_repository_port import ItemRepositoryPort
 from src.domain.ports.item_service_port import ItemServicePort
 from src.domain.services.item_service import ItemService
 
+_item_repository = InMemoryItemRepository()
 
 def get_item_repository() -> ItemRepositoryPort:
-    return InMemoryItemRepository()
+    return _item_repository
 
 
 def get_item_service(
